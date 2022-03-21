1a74d5a28f90e71b5fe287c6f910a85767be3395
cjamesb <cjberry1234@sbcglobal.net>
2022-03-21 15:20:44 -0700

    Changes to file 3

--- 03-jsx/src/App.js
+++ 03-jsx/src/App.js
@@ -1,15 +1,17 @@
-import { render } from "react-dom";
-import Pet from "./Pet";
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
